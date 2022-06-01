de4d0a47b73ce41f97bf3577915371640e04421e
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-06-01 21:42:37 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#325188',
+        light: '#c2d4f2',
+        accent: '#303a4d',
+        white: '#F2F2F2',
+        black: '#121212',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
